811779031d725a4093e137ad0c8764714331c9af
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-01 18:19:48 +0200

    Update trimmer file

--- trimmer.rb
+++ trimmer.rb
@@ -1,7 +1,7 @@
 require 'decorator'
 class TrimmerDecoratorr < Decorator
   def correct_name
-    return unless @nameable.correct_name.size == 10
+    return unless @nameable.correct_name.size <= 10
 
     @nameable.correct_name.strip
   end
