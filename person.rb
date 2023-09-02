eacfac2c5ecda45807bc8dc17579a65f9d7fcfc1
francksefu <90141102+francksefu@users.noreply.github.com>
2023-09-02 08:36:08 +0200

    Update person file

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
 require 'nameable'
+require 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rental
+  attr_accessor :name, :age, :rental
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -10,6 +11,10 @@ class Person < Nameable
     @age = age
     @parent_permission = parent_permission
     @rental = []
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   def correct_name
